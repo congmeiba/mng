2324fc5b9a5d0e1e3d60d08076d95a7b50205e2c
huangxiaosen <hxs_82314976@126.com>
2018-09-23 10:02:27 +0800

    初始化

---
+++ src/main/java/com/nabei/MngApplication.java
@@ -0,0 +1,12 @@
+package com.nabei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MngApplication.class, args);
+	}
+}
